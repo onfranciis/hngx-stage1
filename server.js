74a04d41ad1dd53756cabc923cb42b41de8c3cd7
onfranciis <onukwuf@gmail.com>
2023-09-07 10:09:54 +0100

    feat: major push

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { dayOfTheWeek } = require("./util");
+const app = express();
+const PORT = process.env.PORT || 1234;
+const now = new Date();
+const currentDay = dayOfTheWeek(now.getUTCDay());
+const utc = now.toISOString();
+
+app.get("/", (req, res) => {
+  const { slack_name, track } = req.query;
+
+  if (!slack_name.trim() || !track.trim()) {
+    return res.status(400).send({
+      message: `Missing either "slack_name" or "track" query`,
+    });
+  }
+
+  res.send({
+    slack_name,
+    current_day: currentDay,
+    utc_time: utc,
+    track,
+    github_file_url:
+      "https://www.github.com/onfranciis/hngx-stage-1/blob/main/server.js",
+    github_repo_url: "https://www.github.com/onfranciis/hngx-stage-1",
+    status_code: 200,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`...server is running on port ${PORT}`);
+});
